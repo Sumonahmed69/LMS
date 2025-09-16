8601305582d82063223562aa94c6f399638680d8
Sumon ahmed <sumonahmed012340@gmail.com>
2025-09-16 23:48:21 +0600

    Initial project setup: Next.js + pnpm + shadcn

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,15 @@
+export default function LoginPage() {
+
+    return (
+        <div>
+            <h2>
+                Hello from the login LoginPage
+            </h2>
+
+
+
+
+        </div>
+    )
+
+}
